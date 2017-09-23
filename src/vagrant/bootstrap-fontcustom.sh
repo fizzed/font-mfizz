9a03403d9325b3440038fb291b290cc7cb63e47f
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-09-23 14:37:41 +0200

    Turns out zlib-devel is not needed for sfnt2woff / woff2sfnt to compile

--- src/vagrant/bootstrap-fontcustom.sh
+++ src/vagrant/bootstrap-fontcustom.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y install ruby ruby-dev fontforge ttfautohint unzip libz-dev software-properties-common python-software-properties cmake build-essential zlib-devel oracle-java8-installer
+sudo apt-get -y install ruby ruby-dev fontforge ttfautohint unzip libz-dev software-properties-common python-software-properties cmake build-essential oracle-java8-installer
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo update-java-alternatives -s java-8-oracle
 
