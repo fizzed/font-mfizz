253e5b0f25635316a658adb2801459b229a4602a
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-09-23 16:00:00 +0200

    Never mind me moving the java install. It only worked because vagrant does not loose data, when restarted (unlike docker I am accustomed to).
    Also install fontcustom with some env vars to make error message about encoding go away.

--- src/vagrant/bootstrap-fontcustom.sh
+++ src/vagrant/bootstrap-fontcustom.sh
@@ -1,8 +1,5 @@
-sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y install ruby ruby-dev fontforge ttfautohint unzip libz-dev software-properties-common python-software-properties cmake build-essential oracle-java8-installer
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo update-java-alternatives -s java-8-oracle
+sudo apt-get -y install ruby ruby-dev fontforge ttfautohint unzip libz-dev software-properties-common python-software-properties cmake build-essential
 
 wget https://github.com/Jan-LucaKlees/sfnt2woff/archive/master.zip
 unzip master.zip
@@ -15,4 +12,11 @@ rm -Rf sfnt2woff-master master.zip
 
 # finally fontcustom (can we lock down specific ver?)
 echo "Install ruby gem fontcustom v1.3.8 (may take awhile)..."
-sudo gem install fontcustom -v 1.3.8
+sudo LC_CTYPE=en_US.UTF-8 LANG=en_US.UTF-8 gem install fontcustom -v 1.3.8
+
+# finish off with java 8
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+sudo update-java-alternatives -s java-8-oracle
